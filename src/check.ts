548a6641de43b9d331fdda700ce7d57bc1b6a006
bijianing97 <826015751@qq.com>
2022-07-21 15:10:01 +0800

    chore: add time

--- src/check.ts
+++ src/check.ts
@@ -5,17 +5,32 @@ const provider = new ethers.providers.JsonRpcProvider("http://localhost:11451");
 
 (async () => {
   let blockNumber = await provider.getBlockNumber();
-  console.log("Start to record blockNumber is ", blockNumber);
+  console.log(
+    new Date().toUTCString(),
+    "Start to record blockNumber is ",
+    blockNumber
+  );
 
   while (true) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 120000));
       const blockNumberNow = await provider.getBlockNumber();
-      console.log("Current blockNumber is ", blockNumberNow);
+      console.log(
+        new Date().toUTCString(),
+        "Current blockNumber is: ",
+        blockNumberNow
+      );
       const blockInterval = blockNumberNow - blockNumber;
-      console.log("Block interval is ", blockInterval);
+      console.log(
+        new Date().toUTCString(),
+        "Block interval is: ",
+        blockInterval
+      );
       if (blockInterval < 10) {
-        console.log("Block interval is too short, restart the node");
+        console.log(
+          new Date().toUTCString(),
+          "Block interval is too short, restart the node"
+        );
         shell.exec("pm2 restart rei-fans-node");
       }
       blockNumber = blockNumberNow;
